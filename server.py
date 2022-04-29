ef8c7240f4236e4544d5d84d56f485ba1f5a5137
sipmine <gleb.tedpoope@gmail.com>
2022-04-29 23:51:39 +0700

    feat/add button and guide

--- server.py
+++ server.py
@@ -1,19 +1,35 @@
+from aiogram.types import reply_keyboard
+from aiogram.types.reply_keyboard import KeyboardButton
 from db import create, find_by_id
 from newclient import configFile
 from userconf import createFile 
 
-from safe import BOT_TOKEN
+# from safe import BOT_TOKEN
 
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 
 
+# add button on keyboard
+
+
+# add guide how install vpn
+
 # configure config
 logging.basicConfig(level=logging.INFO)
-print(BOT_TOKEN)
+
 # init bot
+BOT_TOKEN = "5159971668:AAHopjga0bOGUNp3m8eZRZ5eNsvE4hfzHEw"
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
+
+
+button_info = types.KeyboardButton("/guide")
+button_getvpn = types.KeyboardButton("/getvpn")
+main_kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+main_kb.add(button_info)
+main_kb.add(button_getvpn)
+
 print("all commands bot:\n start \n help \n testreg \n")
 # start message 
 @dp.message_handler(commands="start")
@@ -21,12 +37,18 @@ async def cheack_commands(msg: types.message):
     user_id = msg.from_user.id 
     user_name = str(msg.from_user.username)
     create(user_id, user_name)
-    await msg.answer("Привет! \n список мойх функций /help ")
+    await msg.answer("Привет! \n /getvpn даст тебе файл с vpn. \n /guide инструкция по установке", reply_markup=main_kb)
 
 # get info bot
-@dp.message_handler(commands="help")
-async def help_commands(msg: types.message):
-    await msg.answer("Получить доступ к vpn /getvpn")
+
+@dp.message_handler(commands="guide")
+async def guide_commands(msg: types.message):
+    await msg.answer('''И так чтобы пользоваться впн на мобильном устройстве зайди в (AppStore / Google Play) найди там приложение WireGuard. 
+Теперь жмёшь плюсик и нажимаешь импорт из файла (скачай файл, который дал бот). ОБЯЗАТЕЛЬНО СКАЧЕННЫЙ ФАЙЛ ПРЕМЕНУЙТЕ У СЕБЯ НА ТЕЛЕФОНЕ.
+Вот и всё нажимай включить и радуйся :).
+На пк всё просто переходим на этот сайт и устанавливай https://www.wireguard.com/install/ дальше загружаем свой конфиг файл и радуемся''')
+
+
 
 
 # get vpn
@@ -45,5 +67,7 @@ async def getvpn(msg: types.message):
     media.attach_document(types.InputFile(f'{username}_wg.conf'), "vpn file")
     await msg.answer("Ваш файл")
     await msg.answer_media_group(media=media)
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
